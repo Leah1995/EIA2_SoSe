21045fd9a889527cdd697d94613d244843737206
Leah <Leah@LeahHP>
2017-03-19 17:32:14 +0100

    Aufgabe

---
+++ Aufgabe0/Aufgabe0.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var greeting = "Buna ziua";
+    var getName = prompt("Wie heißt du?");
+    document.getElementById("Salut").innerHTML = greeting + " " + getName + "!";
+});
+//# sourceMappingURL=aufgabe0.js.map
