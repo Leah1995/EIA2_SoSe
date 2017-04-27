bcc77a1afff3ab6c916d5c0035b4084642839cb5
Leah <Leah@LeahHP.hfu-open.fu.hs-furtwangen.de>
2017-04-27 12:50:02 +0200

    Verbessert

--- Aufgabe3/A/Aufgabe3a.js
+++ Aufgabe3/A/Aufgabe3a.js
@@ -1,43 +1,43 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var n = 64; // Felder haben die Anzahl 64
-    var size = 120;
-    var reihe = 1;
-    for (var i = 0; i < n; i++) {
-        if (reihe % 2 != 0) { // Modulo anwenden -> wenn ungerade
-            if (i % 2 == 0) {
-                color = "black";
+var L3a_chess;
+(function (L3a_chess) {
+    document.addEventListener("DOMContentLoaded", function () {
+        var n = 64; // Felder haben die Anzahl 64 
+        var size = 120; // hier fehlen überall die Typdeklarationen!
+        var reihe = 1;
+        var color;
+        for (var i = 0; i < n; i++) {
+            if (reihe % 2 != 0) {
+                if (i % 2 == 0) {
+                    color = "black";
+                }
+                else {
+                    color = "white";
+                }
             }
             else {
-                color = "white";
+                if (i % 2 == 0) {
+                    color = "white";
+                }
+                else {
+                    color = "black";
+                }
+            }
+            x = (i % 8) * size; // Modulo anwenden und mit Größe multiplizieren
+            y = (reihe - 1) * size;
+            placeDiv(color, x, y, size, i);
+            if (x == (7 * size)) {
+                reihe++;
             }
         }
-        else {
-            if (i % 2 == 0) { // Modulo anwenden -> wenn gerade
-                color = "white";
-            }
-            else {
-                color = "black";
-            }
+        writerice();
+        var divList = document.getElementsByTagName("div");
+        for (var i = 1; i < 9; i++) {
+            divList[i].addEventListener("click", function () {
+                this.classList.toggle("select");
+                showsumrice();
+            });
         }
-        x = (i % 8) * size; // Modulo anwenden und mit Größe multiplizieren
-        y = (reihe - 1) * size;
-        placeDiv(color, x, y, size, i);
-        if (x == (7 * size)) {
-            reihe++;
-        }
-    }
-    writerice();
-    function writerice() {
-        var feld = document.getElementsByClassName("fields");
-        var rice;
-        for (var j = 0; j < feld.length; j++) {
-            rice = Math.pow(2, j);
-            if (j > 23) {
-                rice = rice.toExponential(5);
-            }
-            feld[j].textContent = rice.toString();
-        }
-    }
+    });
     function placeDiv(_color, _x, _y, _size, _counter) {
         var div = document.createElement("div");
         document.body.appendChild(div);
@@ -51,12 +51,18 @@ document.addEventListener('DOMContentLoaded', function () {
         s.left = _x + "px";
         s.top = _y + "px";
     }
-    var divList = document.getElementsByTagName("div");
-    for (var i_1 = 0; i_1 < 9; i_1++) {
-        divList[i_1].addEventListener("click", function () {
-            this.classList.toggle("select");
-            showsumrice();
-        });
+    function writerice() {
+        var feld = document.getElementsByClassName("fields");
+        var rice;
+        for (var j = 0; j < feld.length; j++) {
+            var divList = document.getElementsByTagName("div");
+            for (var i1 = 0; i1 < 9; i1++) {
+                divList[i1].addEventListener("click", function () {
+                    this.classList.toggle("select");
+                    showsumrice();
+                });
+            }
+        }
     }
     function showsumrice() {
         var selectDivs = document.getElementsByClassName("select");
@@ -67,13 +73,14 @@ document.addEventListener('DOMContentLoaded', function () {
         else {
             document.getElementById("alpha").style.display = "inline-block";
         }
-        for (var i_2 = 0; i_2 < selectDivs.length; i_2++) {
-            sumrice += Number(selectDivs[i_2].textContent);
+        for (var i = 0; i < selectDivs.length; i++) {
+            sumrice += Number(selectDivs[i].textContent);
             document.getElementById("alpha").textContent = "sum of selected rice:" + "\r\n" + "Dezimal: " + sumrice.toString() + "\r\n" + "Hexadezimal: " + sumrice.toString(16);
         }
     }
-});
-document.addEventListener("mousemove", function (Event) {
-    document.getElementById("alpha").style.left = (Event.clientX + 10) + "px";
-    document.getElementById("alpha").style.top = (Event.clientY + 10) + "px";
-});
+    document.addEventListener("mousemove", function (_event) {
+        document.getElementById("alpha").style.left = (_event.clientX + 10) + "px";
+        document.getElementById("alpha").style.top = (_event.clientY + 10) + "px";
+    });
+})(L3a_chess || (L3a_chess = {}));
+//# sourceMappingURL=Aufgabe3a.js.map
--- Aufgabe3/A/Aufgabe3a.ts
+++ Aufgabe3/A/Aufgabe3a.ts
@@ -1,40 +1,92 @@
-document.addEventListener('DOMContentLoaded', function () {
-     var n = 64; // Felder haben die Anzahl 64
-     var size = 120;
- document.addEventListener('DOMContentLoaded', () { // keine anonymen Funktionen nutzen!
-     var n = 64; // Felder haben die Anzahl 64 
-     var size = 120; // hier fehlen überall die Typdeklarationen!
-      var reihe = 1;
-     for (var i = 0; i < n; i++) {
-    let (var i = 0; i < n; i++) {
-          if (reihe % 2 != 0) { // Modulo anwenden -> wenn ungerade
-              if (i % 2 == 0) {
-                  color = "black";
-document.addEventListener('DOMContentLoaded', function () {
-          }
-      }
-      writerice();
-    function writerice() {
-     function writerice() { // Typdeklaration fehlt!
-          var feld = document.getElementsByClassName("fields");
-          var rice;
-          for (var j = 0; j < feld.length; j++) {
-document.addEventListener('DOMContentLoaded', function () {
-      }
-      var divList = document.getElementsByTagName("div");
-      for (var i_1 = 0; i_1 < 9; i_1++) {
-         divList[i_1].addEventListener("click", function () {
-         divList[i_1].addEventListener("click", function () {  // keine anonymen und verschachtelten Funktionen nutzen!
-              this.classList.toggle("select");
-              showsumrice();
-          });
-document.addEventListener('DOMContentLoaded', function () {
-          }
-      }
-  });
- document.addEventListener("mousemove", function (Event) {
- document.addEventListener("mousemove", function (Event) {  // parametername entspricht nicht unseren Stilvorgaben
-      document.getElementById("alpha").style.left = (Event.clientX + 10) + "px";
-      document.getElementById("alpha").style.top = (Event.clientY + 10) + "px";
- }); 
- });
+
+namespace L3a_chess {
+    document.addEventListener("DOMContentLoaded", function(): void { // keine anonymen Funktionen nutzen!
+        let n: number = 64; // Felder haben die Anzahl 64 
+        let size: number = 120; // hier fehlen überall die Typdeklarationen!
+        let reihe: number = 1;
+        let color: string;
+
+        for (let i: number = 0; i < n; i++) {
+            if (reihe % 2 != 0) { // Modulo anwenden -> wenn ungerade
+                if (i % 2 == 0) {
+                    color = "black";
+                }
+                else {
+                    color = "white";
+                }
+            }
+            else {
+                if (i % 2 == 0) { // Modulo anwenden -> wenn gerade
+                    color = "white";
+                }
+                else {
+                    color = "black";
+                }
+            }
+            x = (i % 8) * size; // Modulo anwenden und mit Größe multiplizieren
+            y = (reihe - 1) * size;
+            placeDiv(color, x, y, size, i);
+            if (x == (7 * size)) {
+                reihe++;
+            }
+        }
+        writerice();
+        var divList: NodeListOf<Element> = document.getElementsByTagName("div");
+        for (let i: number = 1; i < 9; i++) {
+            divList[i].addEventListener("click", function(): void {
+                this.classList.toggle("select");
+                showsumrice();
+            });
+        }
+    });
+
+    function placeDiv(_color: string, _x: number, _y: number, _size: number, _counter: number): void {
+        let div: HTMLElement = document.createElement("div");
+        document.body.appendChild(div);
+        let s: CSSStyleDeclaration = div.style;
+        s.position = "absolute";
+        s.display = "inline-block";
+        div.className += _color;
+        div.className += " fields";
+        s.width = _size + "px";
+        s.height = _size + "px";
+        s.left = _x + "px";
+        s.top = _y + "px";
+    }
+
+
+    function writerice(): void {
+        let feld: NodeListOf<Element> = document.getElementsByClassName("fields");
+        let rice: number;
+        for (let j: number = 0; j < feld.length; j++) {
+            let divList: NodeListOf<Element> = document.getElementsByTagName("div");
+            for (let i1: number = 0; i1 < 9; i1++) {
+                divList[i1].addEventListener("click", function(): void {  // keine anonymen und verschachtelten Funktionen nutzen!
+                    this.classList.toggle("select");
+                    showsumrice();
+                });
+            }
+        }
+    }
+
+
+    function showsumrice(): void {
+        let selectDivs: NodeListOf<Element> = document.getElementsByClassName("select");
+        let sumrice: number = 0;
+        if (selectDivs.length == 0) {
+            document.getElementById("alpha").style.display = "none";
+        }
+        else {
+            document.getElementById("alpha").style.display = "inline-block";
+        }
+        for (let i: number = 0; i < selectDivs.length; i++) {
+            sumrice += Number(selectDivs[i].textContent);
+            document.getElementById("alpha").textContent = "sum of selected rice:" + "\r\n" + "Dezimal: " + sumrice.toString() + "\r\n" + "Hexadezimal: " + sumrice.toString(16);
+        }
+    }
+
+    document.addEventListener("mousemove", function(_event: MouseEvent): void {
+        document.getElementById("alpha").style.left = (_event.clientX + 10) + "px";
+        document.getElementById("alpha").style.top = (_event.clientY + 10) + "px";
+    });
+}
