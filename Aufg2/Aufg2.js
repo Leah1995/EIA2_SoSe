39bb92d8982228246a4c2bacab56dcc49f67085e
Leah <Leah@LeahHP.speedport.ip>
2017-10-21 16:48:46 +0200

    Homework

--- Aufg2/Aufg2.js
+++ Aufg2/Aufg2.js
@@ -17,9 +17,39 @@ var L4_Schneelandschaft;
         drawBerg(300, 380, "#c2d6d6", "#f2f2f2");
         drawBerg(700, 350, "#c2d6d6", "#e6e6e6");
         drawBerg(200, 360, "#c2d6d6", "#d9d9d9");
-        drawBaum1(400, 200, "#cc9966", "#44d271");
-        drawBaum2(800, 200, "#4c2e0a", "#00514c");
-        drawFlocke(800, 200);
+        drawBaum1(800, 800, "#cc9966", "#44d271");
+        drawBaum2(800, 800, "#4c2e0a", "#00514c");
+        drawFlocke(800, 600);
+        //Skilift
+        crc2.beginPath();
+        crc2.fillStyle = "black";
+        crc2.fillRect(0, 240, 800, 15); // Anfang, H�he, L�nge, Dicke
+        // Skisitz 1
+        crc2.beginPath();
+        crc2.fillStyle = "black";
+        crc2.fillRect(570, 240, 10, 45);
+        crc2.beginPath();
+        crc2.fillStyle = "black";
+        crc2.fillRect(650, 240, 10, 45);
+        crc2.beginPath();
+        crc2.fillStyle = "black";
+        crc2.fillRect(540, 270, 150, 120);
+        crc2.beginPath();
+        crc2.fillStyle = "#F8FCFE";
+        crc2.fillRect(570, 290, 100, 70);
+        // Skisitz
+        crc2.beginPath();
+        crc2.fillStyle = "black";
+        crc2.fillRect(170, 240, 10, 45);
+        crc2.beginPath();
+        crc2.fillStyle = "black";
+        crc2.fillRect(250, 240, 10, 45);
+        crc2.beginPath();
+        crc2.fillStyle = "black";
+        crc2.fillRect(140, 270, 150, 120);
+        crc2.beginPath();
+        crc2.fillStyle = "#F8FCFE";
+        crc2.fillRect(170, 290, 100, 70);
         // Sonne
         crc2.beginPath();
         crc2.arc(690, 70, 45, 0, 2 * Math.PI); // x, y, size
@@ -115,7 +145,7 @@ var L4_Schneelandschaft;
         // B�ume zuf�llig hinstellen
         for (let i = 0; i < 20; i++) {
             let randomX = (Math.random() * (800 - 1)) + 1;
-            let randomY = (Math.random() * (550 - 400)) + 400;
+            let randomY = (Math.random() * (500 - 400)) + 400;
             let randomBaum1 = Math.floor((Math.random() * 2)) + 1;
             console.log("X is " + randomX, "Y is " + randomY, randomBaum1);
             if (randomBaum1 == 1) {
@@ -128,9 +158,12 @@ var L4_Schneelandschaft;
         // Schneeflocken zuf�llig platzieren
         for (let i = 0; i < 200; i++) {
             let randomX = (Math.random() * (800 - 1)) + 1;
-            let randomY = (Math.random() * (550 - 400)) + 400;
+            let randomY = (Math.random() * (500 - 1)) + 1;
             let randomFlocke = Math.floor((Math.random() * 2)) + 1;
             console.log("X is " + randomX, "Y is " + randomY, randomFlocke);
+            if (randomFlocke == 1) {
+                drawFlocke(randomX, randomY);
+            }
         }
         // Berg
         function drawBerg(_x, _y, _strokeColor, _fillColor) {
@@ -150,24 +183,24 @@ var L4_Schneelandschaft;
         function drawBaum1(_x, _y, _trunkColor, _topColor) {
             // Stamm
             crc2.beginPath();
-            crc2.moveTo(50, 515);
-            crc2.lineTo(75, 565);
-            crc2.lineTo(25, 565);
+            crc2.moveTo(_x, _y);
+            crc2.lineTo(_x + 25, _y + 565);
+            crc2.lineTo(_x - 25, _y + 565);
             crc2.closePath();
             crc2.fillStyle = _trunkColor;
-            crc2.fillRect(124, 467, 12, 23);
+            crc2.fillRect(_x - 5, _y + 40, 12, 23);
             // Baumkrone
             crc2.beginPath();
-            crc2.moveTo(130, 415);
-            crc2.lineTo(155, 455);
-            crc2.lineTo(105, 455);
+            crc2.moveTo(_x + 25, _y + 40);
+            crc2.lineTo(_x - 25, _y + 40);
+            crc2.lineTo(_x, _y);
             crc2.closePath();
             crc2.fillStyle = _topColor;
             crc2.fill();
             crc2.beginPath();
-            crc2.moveTo(130, 425);
-            crc2.lineTo(155, 475);
-            crc2.lineTo(105, 475);
+            crc2.moveTo(_x + 25, _y + 50);
+            crc2.lineTo(_x + 25, _y + 50);
+            crc2.lineTo(_x, _y);
             crc2.closePath();
             crc2.fillStyle = _topColor;
             crc2.fill();
@@ -176,17 +209,17 @@ var L4_Schneelandschaft;
         function drawBaum2(_x, _y, _trunkColor, _topColor) {
             // Stamm
             crc2.beginPath();
-            crc2.moveTo(60, 85);
-            crc2.lineTo(85, 135);
-            crc2.lineTo(35, 135);
-            crc2.closePath();
+            crc2.moveTo(_x, _y);
+            crc2.lineTo(_x + 30, _y - 135);
+            crc2.lineTo(_x - 30, _y - 135);
             crc2.fillStyle = _trunkColor;
-            crc2.fillRect(44, 557, 12, 23);
+            crc2.fillRect(_x - 7, _y + 70, 12, 23);
+            crc2.closePath();
             // Baumkrone
             crc2.beginPath();
-            crc2.moveTo(50, 515);
-            crc2.lineTo(75, 565);
-            crc2.lineTo(25, 565);
+            crc2.moveTo(_x, _y);
+            crc2.lineTo(_x + 20, _y + 70);
+            crc2.lineTo(_x - 20, _y + 70);
             crc2.closePath();
             crc2.fillStyle = _topColor;
             crc2.fill();
@@ -195,53 +228,25 @@ var L4_Schneelandschaft;
         function drawFlocke(_x, _y) {
             // gerade Linie
             crc2.beginPath();
-            crc2.moveTo(355, 30);
-            crc2.lineTo(355, 10);
+            crc2.moveTo(_x, _y - 15);
+            crc2.lineTo(_x, _y + 10);
             crc2.strokeStyle = "white";
             crc2.lineWidth = 5;
             crc2.stroke();
             // schiefe Linien
             crc2.beginPath();
-            crc2.moveTo(365, 26);
-            crc2.lineTo(345, 15);
+            crc2.moveTo(_x - 12, _y + 5);
+            crc2.lineTo(_x + 12, _y - 10);
             crc2.strokeStyle = "white";
+            crc2.lineWidth = 5;
             crc2.stroke();
             crc2.beginPath();
-            crc2.moveTo(365, 15);
-            crc2.lineTo(345, 26);
+            crc2.moveTo(_x + 12, _y + 5);
+            crc2.lineTo(_x - 12, _y - 10);
             crc2.strokeStyle = "white";
+            crc2.lineWidth = 5;
             crc2.stroke();
         }
-        //Skilift
-        crc2.beginPath();
-        crc2.fillStyle = "black";
-        crc2.fillRect(0, 240, 800, 15); // Anfang, H�he, L�nge, Dicke
-        // Skisitz 1
-        crc2.beginPath();
-        crc2.fillStyle = "black";
-        crc2.fillRect(570, 240, 10, 45);
-        crc2.beginPath();
-        crc2.fillStyle = "black";
-        crc2.fillRect(650, 240, 10, 45);
-        crc2.beginPath();
-        crc2.fillStyle = "black";
-        crc2.fillRect(540, 270, 150, 120);
-        crc2.beginPath();
-        crc2.fillStyle = "#F8FCFE";
-        crc2.fillRect(570, 290, 100, 70);
-        // Skisitz
-        crc2.beginPath();
-        crc2.fillStyle = "black";
-        crc2.fillRect(170, 240, 10, 45);
-        crc2.beginPath();
-        crc2.fillStyle = "black";
-        crc2.fillRect(250, 240, 10, 45);
-        crc2.beginPath();
-        crc2.fillStyle = "black";
-        crc2.fillRect(140, 270, 150, 120);
-        crc2.beginPath();
-        crc2.fillStyle = "#F8FCFE";
-        crc2.fillRect(170, 290, 100, 70);
     }
 })(L4_Schneelandschaft || (L4_Schneelandschaft = {}));
 //# sourceMappingURL=Aufg2.js.map
